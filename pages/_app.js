d856930d3efa8b66192b0f653e5eafaeaeffa090
farazmehraien95 <farazmehraien95@gmail.com>
2022-09-14 23:04:19 +0430

    check isLogin on first visit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+import { magic } from '../lib/magic-client'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn()
+      if (isLoggedIn) {
+        // route to /
+        router.push('/')
+      } else {
+        // route to /login
+        router.push('/login')
+      }
+    }
+    handleLoggedIn()
+  }, [])
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setLoading(false)
+    }
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  }, [router])
+
+  return loading ? <div>loading...</div> : <Component {...pageProps} />
 }
 
 export default MyApp
