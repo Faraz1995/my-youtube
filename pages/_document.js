d01b70a910559a34a117fd8317c9e2a949ca7b2a
farazmehraien95 <farazmehraien95@gmail.com>
2022-10-13 14:07:25 +0330

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Amiri&family=Roboto+Slab:wght@700&display=swap'
             rel='stylesheet'
           />
+          <link rel='icon' href='/favicon.png' />
         </Head>
         <body>
           <Main />
